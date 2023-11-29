beaea2a1bf37084ab5671d06855e307e6c781090
Sachmansahni <124330958+Sachmansahni@users.noreply.github.com>
2023-11-29 01:16:14 -0800

    Add files via upload
    upgraded with single parameter

--- main.py
+++ main.py
@@ -1,132 +1,219 @@
 import json
 from fastapi import FastAPI,Path,Query,Body,Depends,HTTPException
-from models import users
-from mongoengine import connect
-from mongoengine.queryset.visitor import Q 
-from random import random
+from mongoengine import connect,Q
+from models import Medicines
 from pydantic import BaseModel
-from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
-from datetime import timedelta,datetime
-from jose import jwt
-pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
-
-oauth2_scheme=OAuth2PasswordBearer(tokenUrl="token")
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-
-def authenticate_user(username,password):
-    try:
-        user=json.load(users.objects.get(username=username).to_json())
-        password_check=pwd_context.verify(["password"])
-        return password_check
-    except users.DoesNotExist:
-        return False 
+from urllib.parse import quote_plus
 
 app=FastAPI()
-connect(db="mycollection",host="localhost",port=27017)
 
-SECRET_KEY='COPY THE SECRET KEY HERE CREATED BY USING opensslm rand -hex 32'
-ALGORITHM="HS256"
-def create_access_token(data:dict,expires_delta:timedelta):
-    to_encode=data.copy()
-    expire=datetime.utcnow()+expires_delta
-    to_encode.update({"exp":expire})
-    encoded_jwt=jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
-    return encoded_jwt
+connect(db="medofin_medicine",host="localhost",port=27017)
 
-@app.post("/token")
-def login(form_data:OAuth2PasswordRequestForm=Depends()):
-     username=form_data.username
-     password=form_data.password
-     if authenticate_user(username,password):
-        access_token=create_access_token(
-            data={"sub":username},expires_delta=timedelta(minutes=30)
+class NewMedicine(BaseModel):
+    med_id:int
+    name:str
+    manufacturers:str
+    salt_composition:str
+    medicine_type:str
+    stock:str
+    primary_use:str
+    packaging:str
+    package:str
+    quantity:int
+    product_form:str
+    mrp:int
+    country_of_origin:str
+
+
+
+@app.post("/add_medicine")
+def add_medicine(medicine: NewMedicine):
+    try:
+        new_medicine = Medicines(
+            med_id=medicine.med_id,
+            name=medicine.name,
+            manufacturers=medicine.manufacturers,
+            salt_composition=medicine.salt_composition,
+            medicine_type=medicine.medicine_type,
+            stock=medicine.stock,
+            primary_use=medicine.primary_use,
+            packaging=medicine.packaging,
+            package=medicine.package,
+            quantity=medicine.quantity,
+            product_form=medicine.product_form,
+            mrp=medicine.mrp,
+            country_of_origin=medicine.country_of_origin
         )
-        return{"access_token":access_token,"token_type":"bearer"}
-     else:
-        raise HTTPException(status_code=400,detail="incorrect username or password")
-     
-
-@app.get("/")
-def home(token:str=Depends(oauth2_scheme)):
-    return{"message":"hello world"}
+        new_medicine.save()
+        return {"message": "Medicine added successfully", "medicine_id": str(new_medicine.id)}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
 
 
+@app.get("/get_all_medicines")
+def get_all_medicines():
+    medicines=Medicines.objects().to_json()
+    medicine_list=json.loads(medicines)
+    return{"Medicines":medicine_list}
 
 
-@app.get("/get_all_users")
-def get_all_users():
-    Users=users.objects().to_json()
-    user_list=json.loads(Users)
-    return{"users":user_list}
+from urllib.parse import quote_plus
 
+# ...
 
+import re
 
+# ...
 
-@app.get("/get_users/{user_id}")
-def get_user(user_id:int=Path(...,gt=0)):
-    User=users.objects.get(user_id=user_id)
-    user_dict={
-        "user_id":User.user_id,
-        "username":User.username,
-        "password":User.password
-    }
-    return {"user":user_dict.username}
+@app.get("/get_medicines_by_salt/{salt_name}")
+def get_medicines_by_salt(salt_name: str = Path(..., title="Salt Name")):
+   
+    escaped_salt_name = re.escape(salt_name)    #re.escape escape the special characters in the salt 
+    medicines = Medicines.objects(salt_composition__iregex=f".*{escaped_salt_name}.*")    #f".*{ecaped_salt_name},*" is the regular expression pattern,it matches any part of the salt_composition
+    
+    # Convert the queryset to a list of dictionaries
+    medicines_list = [
+        {
+            "med_id": medicine.med_id,
+            "name": medicine.name,
+            "manufacturers": medicine.manufacturers,
+            "salt_composition": medicine.salt_composition,
+            "medicine_type": medicine.medicine_type,
+            "stock": medicine.stock,
+            "primary_use": medicine.primary_use,
+            "packaging": medicine.packaging,
+            "package": medicine.package,
+            "quantity": medicine.quantity,
+            "product_form": medicine.product_form,
+            "mrp": medicine.mrp,
+            "country_of_origin": medicine.country_of_origin
+        }
+        for medicine in medicines
+    ]
+    
+    if not medicines_list:
+        raise HTTPException(status_code=404, detail="No medicines found with the specified salt.")
 
-
-@app.get("/get_otp/{username}")
-def get_employee(username):                        
-    user=users.objects.get(username=username)             
-    returnotp=0
-    if user:
-        returnotp=user.otp
-        newotp=otp()
-        user.otp = newotp
-        user.save()
-        return {"message": "OTP updated successfully"}
-    return {"message": "User not found"}
-    return returnotp
-
-
-
-@app.get("/get_userpassword/{username}")
-def get_employee(username):                        
-	user=users.objects.get(username=username)             
-	employee_dict={
-		"password":user.password
-		}
-	return employee_dict
+    return {"medicines": medicines_list}
 
 
 
-class NewUser(BaseModel):
-    username:str
-    password:str
-    user_id:int
-    otp:int
 
-def otp():
-     return random()*1000000
 
-@app.post("/add_user")
-def add_user(user:NewUser):
-	new_user=users(username=user.username,
-				password=get_password_hash(user.password),
-				user_id=user.user_id,
-				otp=otp())
-	new_user.save()
-	return{"message":"user added successfully"}
 
-#   updating the otp 
-@app.put("/update_otp/{username}")
-def update_otp(username: str):
-    user = users.objects.get(username=username)
-    if user:
-        newotp=otp()
-        user.otp = newotp
-        user.save()
-        return {"message": "OTP updated successfully"}
-    return {"message": "User not found"}
+@app.get("/get_medicines_by_name/{name}")
+def get_medicines_by_name(name: str = Path(..., title="Name")):
+   
+    escaped_name = re.escape(name)   
+    medicines = Medicines.objects(name__iregex=f".*{escaped_name}.*")   
+    
+    # Convert the queryset to a list of dictionaries
+    medicines_list = [
+        {
+            "med_id": medicine.med_id,
+            "name": medicine.name,
+            "manufacturers": medicine.manufacturers,
+            "salt_composition": medicine.salt_composition,
+            "medicine_type": medicine.medicine_type,
+            "stock": medicine.stock,
+            "primary_use": medicine.primary_use,
+            "packaging": medicine.packaging,
+            "package": medicine.package,
+            "quantity": medicine.quantity,
+            "product_form": medicine.product_form,
+            "mrp": medicine.mrp,
+            "country_of_origin": medicine.country_of_origin
+        }
+        for medicine in medicines
+    ]
+    
+    if not medicines_list:
+        raise HTTPException(status_code=404, detail="No medicines found with the specified name.")
+
+    return {"medicines": medicines_list}
+
+
+
+
+
+
+@app.get("/get_medicines_by_symptom/{symptom}")
+def get_medicines_by_symptom(symptom: str = Path(..., title="Symptom")):
+   
+    escaped_symptom = re.escape(symptom)    #re.escape escape the special characters in the salt 
+    medicines = Medicines.objects(primary_use__iregex=f".*{escaped_symptom}.*")    #f".*{ecaped_salt_name},*" is the regular expression pattern,it matches any part of the salt_composition
+    
+    # Convert the queryset to a list of dictionaries
+    medicines_list = [
+        {
+            "med_id": medicine.med_id,
+            "name": medicine.name,
+            "manufacturers": medicine.manufacturers,
+            "salt_composition": medicine.salt_composition,
+            "medicine_type": medicine.medicine_type,
+            "stock": medicine.stock,
+            "primary_use": medicine.primary_use,
+            "packaging": medicine.packaging,
+            "package": medicine.package,
+            "quantity": medicine.quantity,
+            "product_form": medicine.product_form,
+            "mrp": medicine.mrp,
+            "country_of_origin": medicine.country_of_origin
+        }
+        for medicine in medicines
+    ]
+    
+    if not medicines_list:
+        raise HTTPException(status_code=404, detail="No medicines found with the specified salt.")
+
+    return {"medicines": medicines_list}
+
+
+
+
+
+
+
+
+@app.get("/search_medicines")
+def search_medicines(
+    search_query: str = Query(None, title="Search Query")
+):
+    # Define a query based on the provided parameter
+    query = Q()
+
+    if search_query:
+        escaped_search_query = re.escape(search_query)
+        query = (
+            Q(salt_composition__iregex=f".*{escaped_search_query}.*") |
+            Q(name__iregex=f".*{escaped_search_query}.*") |
+            Q(primary_use__iregex=f".*{escaped_search_query}.*")
+        )
+
+    # Perform the database query
+    medicines = Medicines.objects(query)
+
+    # Convert the queryset to a list of dictionaries
+    medicines_list = [
+        {
+            "med_id": medicine.med_id,
+            "name": medicine.name,
+            "manufacturers": medicine.manufacturers,
+            "salt_composition": medicine.salt_composition,
+            "medicine_type": medicine.medicine_type,
+            "stock": medicine.stock,
+            "primary_use": medicine.primary_use,
+            "packaging": medicine.packaging,
+            "package": medicine.package,
+            "quantity": medicine.quantity,
+            "product_form": medicine.product_form,
+            "mrp": medicine.mrp,
+            "country_of_origin": medicine.country_of_origin
+        }
+        for medicine in medicines
+    ]
+
+    if not medicines_list:
+        raise HTTPException(status_code=404, detail="No medicines found with the specified criteria.")
+
+    return {"medicines": medicines_list}
--- models.py
+++ models.py
@@ -1,6 +1,16 @@
-from mongoengine import Document,StringField,IntField
-class users (Document):
-    username=StringField()
-    password=StringField()
-    user_id=IntField()
-    otp=IntField()
+from mongoengine import Document, StringField, IntField
+
+class Medicines(Document):
+    med_id = IntField()
+    name = StringField()
+    manufacturers = StringField()
+    salt_composition = StringField()
+    medicine_type = StringField()
+    stock = StringField()
+    primary_use = StringField()
+    packaging = StringField()
+    package = StringField()
+    quantity = IntField()
+    product_form = StringField()
+    mrp = IntField()
+    country_of_origin = StringField()
